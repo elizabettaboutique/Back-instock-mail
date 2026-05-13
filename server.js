04be3225227ed11d5c520ed2b06b2b479447f761
elizabettaboutique <francesca@elizabetta.net>
2026-05-13 16:19:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   }
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
 
